9f4911337ef950e8fa5d1e663c303fbd1df744d3
gaba-work <malkocmax@gmail.com>
2025-07-17 12:20:12 +0300

    edit from html

---
+++ front-page.php
@@ -0,0 +1,11 @@
+<?php
+/*
+ * Template Name: front-page
+ */
+
+get_header();
+?>
+
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium beatae cupiditate, hic illum iure molestias nesciunt nobis rem sequi soluta totam ullam! Deserunt dolorem fugit unde. Adipisci, corporis voluptatum? Praesentium?
+
+<?php get_footer(); ?>
