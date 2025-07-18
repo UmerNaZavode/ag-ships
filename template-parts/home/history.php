3326a7503c639bbb0b61776a90166638dab67d2f
gaba-work <malkocmax@gmail.com>
2025-07-18 12:21:04 +0300

    finished history and publications sections

--- template-parts/home/history.php
+++ template-parts/home/history.php
@@ -1,67 +1,40 @@
+<?php
+$history = get_field("history");
+$title = $history["title"];
+$description = $history["description"];
+$path_image = $history["path_image"];
+$path_info = $history["path_info"];
+?>
+
 <section class="history">
     <div class="history__inner container">
         <header class="history__header">
-            <h3 class="history__title title title--alt">История компании</h3>
+            <h3 class="history__title title title--alt"><?php echo $title; ?></h3>
         </header>
         <div class="history__body">
             <div class="history__description">
                 <div class="history__info">
-                    <p>
-                        Congue vel ac parturient placerat vel lectus. Turpis in interdum feugiat enim purus
-                        venenatis ornare aliquam nunc. Commodo id ac amet purus tortor suscipit congue
-                        facilisis. Tempus bibendum
-                        viverra est vel. Ullamcorper dignissim donec feugiat porta. Interdum neque porttitor
-                        ultrices feugiat sit
-                        nulla
-                        ullamcorper nibh amet. Imperdiet risus gravida tempus suspendisse nulla aliquam est. Sed
-                        quis in maecenas
-                        vestibulum neque malesuada scelerisque mauris. Pharetra ipsum sapien vestibulum fames.
-                        Nisl phasellus
-                        pellentesque quam risus non orci.
-                    </p>
-                    <p>
-                        Risus magna mauris massa gravida turpis vestibulum vulputate libero scelerisque. Proin
-                        sed quam amet
-                        pellentesque rhoncus semper aenean ultrices gravida. Tristique scelerisque lorem sed
-                        eget integer sed sed.
-                        Leo
-                        donec mi sagittis ac adipiscing diam felis quisque diam. Auctor sed vulputate odio non
-                        velit amet. At justo
-                        porta gravida elementum mi. Neque tellus tellus sit etiam volutpat aliquam tincidunt
-                        semper. Nibh lobortis
-                        commodo sapien sollicitudin sagittis. Turpis erat amet volutpat urna quis felis. Lorem
-                        mi amet dolor dictum.
-                        Facilisis dignissim mauris tristique adipiscing molestie eget consectetur nec non. Id
-                        elementum nibh
-                        faucibus
-                        enim pharetra faucibus ultrices neque magnis. At cursus accumsan blandit tellus nisi
-                        tincidunt sit. Lacus
-                        mattis
-                        mattis nunc tincidunt vitae et.
-                    </p>
-                    <p>
-                        Orci augue morbi bibendum ac viverra diam a fermentum. Massa eget porttitor tellus
-                        aliquam quisque turpis at
-                        nunc. Ut tortor quam urna suscipit. Dictumst neque tincidunt malesuada facilisi cras
-                        urna. In nunc sed
-                        ultrices
-                        eu parturient aliquam. Non lorem facilisis tincidunt est aliquam hac diam. Suspendisse
-                        viverra eleifend leo
-                        facilisis in. Id purus arcu cursus id. Pretium semper id sit arcu id adipiscing. Neque.
-                    </p>
+
+                    <?php foreach ($description as $item): ?>
+                        <?php
+                        $info = $item["info"];
+                        ?>
+                        <p><?php echo $info; ?></p>
+                    <?php endforeach; ?>
                 </div>
             </div>
             <div class="history__path">
-                <img src="/assets/history-BtHVx_hU.svg" width="638" height="406" alt=""
+                <img src="<?php echo $path_image; ?>" width="638" height="406" alt=""
                      class="history__path-img">
                 <div class="history__path-info">
                     <ul class="history__path-list">
-                        <li class="history__path-item">Etiam hendrerit aliquam mattis mauris at orci.</li>
-                        <li class="history__path-item">Risus integer ultrices tincidunt massa.</li>
-                        <li class="history__path-item">Scelerisque lorem semper facilisi auctor metus eros in.
-                            Feugiat.
-                        </li>
-                        <li class="history__path-item">Nec id diam varius laoreet volutpat arcu tempus.</li>
+                        <?php foreach ($path_info as $item): ?>
+                            <?php
+                            $info = $item["info"];
+                            ?>
+                            <li class="history__path-item"><?php echo $info; ?></li>
+                        <?php endforeach; ?>
+
                     </ul>
                 </div>
             </div>
--- template-parts/home/news.php
+++ template-parts/home/news.php
@@ -13,30 +13,30 @@ $items = $news["items"];
         </header>
         <div class="news__body">
             <ul class="news__list">
+                <?php foreach ($items
 
-                <?php foreach($items as $item): ?>
-                  <?php
-                      $image = $item["image"];
-                      $date = $item["date"];
-                      $subtitle = $item["subtitle"];
-                      $description = $item["description"];
-                  ?>
+                as $item): ?>
 
-                    <li class="news__item">
-                        <img src="<?php echo $image; ?>" width="306" height="330" alt=""
-                             class="news__item-img">
-                        <p class="news__item-date" datetime="2022-12-29"><?php echo $date; ?></p>
-                        <div class="news__item-body">
-                            <a href="/" class="news__item-link">
-                                <h4 class="news__item-subtitle"><?php echo $subtitle; ?></h4>
-                            </a>
-                            <p class="news__item-description"><?php echo $description; ?></p>
-                        </div>
-                    </li>
+                <?php
+                $image = $item["image"];
+                $date = $item["date"];
+                $subtitle = $item["subtitle"];
+                $description = $item["description"];
+                ?>
 
-                <?php endforeach; ?>
+                <li class="news__item">
+                    <img src="<?php echo $image; ?>" width="306" height="330" alt=""
+                         class="news__item-img">
+                    <p class="news__item-date" datetime="2022-12-29"><?php echo $date; ?></p>
+                    <div class="news__item-body">
+                        <a href="/" class="news__item-link">
+                            <h4 class="news__item-subtitle"><?php echo $subtitle; ?></h4>
+                        </a>
+                        <p class="news__item-description"><?php echo $description; ?></p>
+                    </div>
+
+                    <?php endforeach; ?>
             </ul>
         </div>
     </div>
 </section>
-
