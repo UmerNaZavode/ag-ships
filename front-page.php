1cbb27dcb058a84ddae522460e65543033ace418
gaba-work <malkocmax@gmail.com>
2025-07-21 10:56:03 +0300

    upd: formated activities and fron-page

--- front-page.php
+++ front-page.php
@@ -8,12 +8,12 @@ get_header();
 
 <?php echo get_template_part("template-parts/home/hero-section"); ?>
 
-    <main>
-        <?php echo get_template_part("template-parts/home/news"); ?>
-        <?php echo get_template_part("template-parts/home/history"); ?>
-        <?php echo get_template_part("template-parts/home/publications"); ?>
-        <?php echo get_template_part("template-parts/home/projects"); ?>
-        <?php echo get_template_part("template-parts/home/safety"); ?>
-    </main>
+<main>
+  <?php echo get_template_part("template-parts/home/news"); ?>
+  <?php echo get_template_part("template-parts/home/history"); ?>
+  <?php echo get_template_part("template-parts/home/publications"); ?>
+  <?php echo get_template_part("template-parts/home/projects"); ?>
+  <?php echo get_template_part("template-parts/home/safety"); ?>
+</main>
 
 <?php get_footer(); ?>
