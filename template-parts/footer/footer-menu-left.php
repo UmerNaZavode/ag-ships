1a9c699b2289c5d64783a0f6672ac74facf12e0c
gaba-work <malkocmax@gmail.com>
2025-07-21 10:56:21 +0300

    feat: created footer menu files

---
+++ template-parts/footer/footer-menu-left.php
@@ -0,0 +1,36 @@
+<?php wp_nav_menu([
+  'theme_location' => 'footer-left',
+  'menu' => '',
+  'container' => '',
+  'container_class' => '',
+  'container_id' => '',
+  'menu_class' => 'footer__list',
+  'menu_id' => 'js-main-menu',
+  'echo' => true,
+  'fallback_cb' => 'wp_page_menu',
+  'before' => '',
+  'after' => '',
+  'link_before' => '',
+  'link_after' => '',
+  'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul>',
+  'depth' => 0,
+  'walker' => '',
+]); ?>
+
+<div class="footer__column">
+  <a href="/" class="footer__title">О компании</a>
+  <!-- <ul class="footer__list"> -->
+  <!--   <li class="footer__item"> -->
+  <!--     <a href="" class="footer__link">Профиль компании</a> -->
+  <!--   </li> -->
+  <!--   <li class="footer__item"> -->
+  <!--     <a href="" class="footer__link">История</a> -->
+  <!--   </li> -->
+  <!--   <li class="footer__item"> -->
+  <!--     <a href="" class="footer__link">Совет директоров</a> -->
+  <!--   </li> -->
+  <!--   <li class="footer__item"> -->
+  <!--     <a href="" class="footer__link">Контакты</a> -->
+  <!--   </li> -->
+  <!-- </ul> -->
+</div>
